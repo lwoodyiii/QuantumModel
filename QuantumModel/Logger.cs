6296d8e0c1b41228337663f6550fe6e8aa9e83e7
Leonard Woody <leonard.woody@gmail.com>
2019-12-23 23:50:40 -0500

    Changed solution structu
    Changed solution structu

---
+++ QuantumModel/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantumModel
+{
+    class Logger
+    {
+        public Logger()
+        { 
+        
+        }
+        
+        public void w(string l)
+        {
+            Console.WriteLine(l);
+
+        }
+
+        internal void L(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+}
---
+++ QuantumModelTests/KetTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuantumModel;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace QuantumModel.Tests
+{
+    [TestClass()]
+    public class KetTests
+    {
+        [TestMethod()]
+        public void DistrubiveWithScalars()
+        {
+            Ket k1 = new Ket(new Complex[]{ new Complex(2,2), 2 }, "k1");
+            Ket k2 = new Ket(new Complex[]{ new Complex(2,2), 2 }, "k2");
+            Ket expected = (3+2) * k1;
+
+
+
+
+            Assert.AreEqual(expected, 3 * k1 + 2 * k1);
+        }
+
+
+    }
+}
